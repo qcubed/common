9ea96fb9948a0b57711da25c937e1ddc2a8c4b67
spekary <spekary@gmail.com>
2017-04-25 11:04:20 -0700

    travis fix

--- test/travis-config.inc.php
+++ test/travis-config.inc.php
@@ -3,6 +3,6 @@
 /**
  * The base configuration file for the travis test
  */
-require ('../../vendor/autoload.php');
+require ('./vendor/autoload.php');
 
 define('__APPLICATION_ENCODING_TYPE__', 'UTF-8');
